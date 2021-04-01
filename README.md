50f56ec233c0d87101c4241571e6173a3eea2b61
Chenqi <1002652154@qq.com>
2021-04-01 12:16:21 +0800

    solution created

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # TextEditor
 A test example for WndDesign and BlockStore.
+
+
+
+Open with Visual Studio 2019.
+
+Below are all configurations modified from default:
+
+	General
+		Output Directory: $(SolutionDir)build\$(platform)\$(Configuration)\
+		Intermidiate Directory: $(SolutionDir)temp\$(ProjectName)\$(Platform)\$(Configuration)\
+		C++ Language Standard: ISO C++17 Standard
+	
+	C/C++
+		General
+			Additional Include Directories: (use your own directory)
+				E:\source\WndDesign\code; 
+				E:\source\BlockStore\code;
+				%(AdditionalIncludeDirectories)
+	
+	Linker
+		General
+			Additional Libaray Directories: $(OutputPath)
+
+
+And put these library files in *Additional Libaray Directories*, or $(OutputPath):
+
+```
+WndDesign.lib, WndDesignCore.lib, WndDesignCore.dll,
+StaticStore.lib, StaticStore.dll	
+```
+
+
+
+Now you can build and run the program.
---
+++ code/TextEditor/main.cpp
@@ -0,0 +1,55 @@
+#include "WndDesign/WndDesign.h"
+#include "WndDesign/wnd/EditBox.h"
+#include "StaticStore/StaticStore.h"
+
+
+using namespace WndDesign;
+using namespace StaticStore;
+
+
+#pragma comment(lib, "WndDesign.lib")
+#pragma comment(lib, "StaticStore.lib")
+
+
+class TextArea : public EditBox {
+private:
+	struct Style : EditBox::Style {
+		Style() {
+			width.setFixed(500px);
+			height.max(100pct);
+			position.setHorizontalCenter().top(100px);
+			border.width(3).color(ColorSet::Honeydew).setResizer(CreateAeroSnapBorderResizer());
+			padding.set(20px, 10px, 20px, 10px);
+			background.setColor(ColorSet::YellowGreen);
+			font.size(20);
+		}
+	};
+public:
+	TextArea(Engine& engine) : 
+		EditBox(std::make_unique<Style>()),
+		engine(engine),
+		meta_index(engine.GetMetadata<ArrayIndex>()) {
+		Array<wchar> text_array(engine, meta_index);
+		wstring text; text.resize(text_array.GetLength()); text_array.Load(text.data());
+		SetText(std::move(text));
+	}
+	~TextArea() {
+		Array<wchar> text_array(engine, meta_index);
+		engine.Format();
+		const wstring& text = GetText();
+		text_array.Store(text.data(), text.length());
+		engine.SetMetadata<ArrayIndex>(meta_index);
+	}
+private:
+	Engine& engine;
+	ArrayIndex meta_index;
+};
+
+
+int main() {
+	auto engine = Engine::Create(L"test.te");
+	TextArea text_area(*engine);
+	desktop.AddChild(text_area);
+	desktop.MessageLoop();
+	return 0;
+}
