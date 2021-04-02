555c37953c7615be3997e82ccf2b64c8cf9bef5d
Chenqi <1002652154@qq.com>
2021-04-02 19:50:50 +0800

    structure updated

--- code/TextEditor/main.cpp
+++ code/TextEditor/main.cpp
@@ -11,45 +11,74 @@ using namespace StaticStore;
 #pragma comment(lib, "StaticStore.lib")
 
 
+struct TextAeraData {
+	ArrayIndex<wchar> index;
+};
+
+
 class TextArea : public EditBox {
 private:
 	struct Style : EditBox::Style {
 		Style() {
-			width.setFixed(500px);
-			height.max(100pct);
+			width.normal(500px).max(100pct);
+			height.normal(70pct).max(100pct);
 			position.setHorizontalCenter().top(100px);
-			border.width(3).color(ColorSet::Honeydew).setResizer(CreateAeroSnapBorderResizer());
+			border.width(3).color(ColorSet::Black).setResizer(CreateAeroSnapBorderResizer());
 			padding.set(20px, 10px, 20px, 10px);
-			background.setColor(ColorSet::YellowGreen);
-			font.size(20);
+			background.setColor(ColorSet::White);
+			paragraph.line_height(130pct).tab_size(200pct);
+			font.family(L"µ»œﬂ").size(20);
 		}
 	};
+
 public:
-	TextArea(Engine& engine) : 
-		EditBox(std::make_unique<Style>()),
-		engine(engine),
-		meta_index(engine.GetMetadata<ArrayIndex>()) {
-		Array<wchar> text_array(engine, meta_index);
-		wstring text; text.resize(text_array.GetLength()); text_array.Load(text.data());
-		SetText(std::move(text));
+	TextArea() : EditBox(std::make_unique<Style>()) {}
+	~TextArea() {}
+
+public:
+	void Load(Engine& engine, TextAeraData data) {
+		Array<wchar> text_array(engine, data.index); 
+		auto [text, length] = text_array.Load();
+		SetText(wstring(text, length));
 	}
-	~TextArea() {
-		Array<wchar> text_array(engine, meta_index);
-		engine.Format();
+	const TextAeraData Save(Engine& engine) {
+		TextAeraData data;
+		Array<wchar> text_array(engine, data.index);
 		const wstring& text = GetText();
 		text_array.Store(text.data(), text.length());
-		engine.SetMetadata<ArrayIndex>(meta_index);
+		return data;
+	}
+};
+
+
+class GlobalManager {
+private:
+	static constexpr wchar file_name[] = L"test.te";
+	TextArea text_area;
+public:
+	GlobalManager() { Load(); }
+	~GlobalManager() { Save(); }
+public:
+	void Run() {
+		desktop.AddChild(text_area);
+		desktop.MessageLoop();
 	}
 private:
-	Engine& engine;
-	ArrayIndex meta_index;
+	void Load() {
+		unique_ptr<Engine> engine = Engine::Create(file_name);
+		TextAeraData data = engine->GetMetadata<TextAeraData>();
+		text_area.Load(*engine, data);
+	}
+	void Save() {
+		unique_ptr<Engine> engine = Engine::Create(file_name);
+		engine->Format();
+		TextAeraData data = text_area.Save(*engine);
+		engine->SetMetadata<TextAeraData>(data);
+	}
 };
 
 
 int main() {
-	auto engine = Engine::Create(L"test.te");
-	TextArea text_area(*engine);
-	desktop.AddChild(text_area);
-	desktop.MessageLoop();
-	return 0;
+	GlobalManager global_manager;
+	global_manager.Run();
 }
